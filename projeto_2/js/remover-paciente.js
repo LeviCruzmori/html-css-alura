b890b051218239aa214cc9a8ef20bcc12c1c1f72
levimori <dsciencemori@gmail.com>
2020-01-15 14:52:01 -0300

    add arquivo filtrar tabela

--- projeto_2/js/remover-paciente.js
+++ projeto_2/js/remover-paciente.js
@@ -1,1 +1,22 @@
-var pacientes = document.querySelectorAll('.paciente');
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector('table');
+
+tabela.addEventListener('dblclick',function(event){
+    event.target.parentNode.classList.add('fadeOut');
+    
+    setTimeout(function(){
+       event.target.parentNode.remove(); 
+    },500);
+    
+    
+})
+
+/* colocando envento  este evento so'se refere au elemento clicado*//*
+pacientes.forEach(function(paciente){
+    paciente.addEventListener('dblclick', function(){
+        console.log('fui clicado com duplo clikc')
+        this.remove();
+    })
+})
+*/
